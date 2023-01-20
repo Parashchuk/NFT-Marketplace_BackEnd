fdbab9678e8dabeb9bcc40ece6fc035b06646dbb
Andrii Parashchuk <anypaster@Andriis-MacBook-Pro.local>
2023-01-20 15:21:26 +0100

    optimise images and update BD

--- models/user.js
+++ models/user.js
@@ -1,44 +1,48 @@
 import mongoose from 'mongoose';
 
 const User = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    email: {
-        type: String,
-        require: true,
-        unique: true,
-    },
-    passwordHash: {
-        type: String,
-        require: true,
-    },
-    avatar: {
-        type: String,
-        default: '',
-    },
-    totalSales: {
-        type: Number,
-        default: 0,
-    },
-    volume: {
-        type: Number,
-        default: 0,
-    },
-    soldNfts: {
-        type: Number,
-        default: 0,
-    },
-    followersCount: {
-        type: Number,
-        default: 0,
-    },
-    bio: {
-        type: String,
-        default: '',
-    },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  passwordHash: {
+    type: String,
+    require: true,
+  },
+  avatar: {
+    type: String,
+    default: null,
+  },
+  hedaerImage: {
+    type: String,
+    default: null,
+  },
+  totalSales: {
+    type: Number,
+    default: 0,
+  },
+  volume: {
+    type: Number,
+    default: 0,
+  },
+  soldNfts: {
+    type: Number,
+    default: 0,
+  },
+  followersCount: {
+    type: Number,
+    default: 0,
+  },
+  bio: {
+    type: String,
+    default: '',
+  },
 });
 
 export default mongoose.model('User', User);
